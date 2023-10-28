f63c6911f80299aa25736bb2a6775205e7dafe01
ttomm <to_scan@autograf.pl>
2023-10-28 22:14:28 +0200

    from vs defer

--- index.ts
+++ index.ts
@@ -1,27 +1,43 @@
-import { from } from 'rxjs';
+import { from, fromEvent, defer } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
-// uruchomione zostaną 4 emit'y: 3 next'y, po jednym dla każdej wartości z poniższej tablicy, i jeden complete
-from([
-  1,
-  'qwe',
-  new Error('upssss'), // nie powoduje uruchomienia poniższego listenera "error" w Observer'ze, tylko normalnie "next'a"
-]).subscribe({
-  next: (data) => console.log('Data:', data),
-  error: (er) => console.error(er),
-  complete: () => console.log('Completed'),
-});
+const btn = document.querySelector('#run');
 
-const promise = new Promise<number>((resolve, reject) => {
-  const result = Math.floor(Math.random() * 10);
+// Observable tworzony z Promise'a za pomocą from() jest uruchamiany od razu (funkcja getPromise()
+// wywoływana jest od razu) i każde kliknięcie na btn#run zwraca ten sam wynik, czyli random number
+// wylosowany przy uruchomieniu Promise'a
 
-  if (result > 5) {
-    setTimeout(() => resolve(result), 3000);
-  } else {
-    setTimeout(() => reject(result), 2500);
-  }
-});
+// Observable tworzony z Promise'a za pomocą defer() NIE jest uruchamiany od razu,
+//  każde kliknięcie na btn#run powoduje NOWE wywoływanie funkcji getPromise() i tym samym utworzenie
+// nowego Promise'a i, co za tym idzie, wylosowanie nowego random number
 
-from<Promise<number>>(promise).subscribe({
-  next: (data: number) => console.log('Promised number:', data),
-  error: (err: number) => console.error('Not enought:', -err),
-});
+// Trzeci przypadek, czyli wstawienie fynkcji getPromise() bezpośrednio do switchMap(), powoduje takie
+// samo zachowanie jak w przypadku defer
+
+// const fromPromiseObservable = from(getPromise());
+const deferPromiseObservbale = defer(() => getPromise());
+
+fromEvent(btn, 'click')
+  .pipe(
+    // switchMap(() => fromPromiseObservable)
+    switchMap(() => deferPromiseObservbale)
+    // switchMap(() => getPromise())
+  )
+  .subscribe({
+    next: (v) => console.log('CLICK', v),
+    error: (err) => console.error('ERR', err),
+    complete: () => console.info('COMPLETE'),
+  });
+
+function getPromise() {
+  console.log('GET PROMISE');
+  return new Promise<number>((resolve, reject) => {
+    const result = Math.floor(Math.random() * 10);
+    console.info('PROMISE PREPARED with: ', result);
+
+    setTimeout(() => {
+      console.info('PROMISE RUN with: ', result);
+      resolve(result);
+    }, 3000);
+  });
+}
